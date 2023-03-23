d69fb163ba655c774f2d1135030d4ab760395a8b
Paillat <git@electronique.cc>
2023-03-23 18:28:22 +0100

    A

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
-    if (window.scrollY > 100) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
-  
